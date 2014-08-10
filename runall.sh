bde5c652315dc537dce50b85c8c2447e43a1b542
Ralph Yozzo <fedex1@gmail.com>
2014-08-09 20:31:44 -0400

    add automatic process

--- runall.sh
+++ runall.sh
@@ -3,7 +3,7 @@ FILENAME=dob_`date +%Y-%m-%d`.csv
 HEADERFILENAME=header_dob_`date +%Y-%m-%d`.csv
 RESULTFILENAME=result_dob_`date +%Y-%m-%d`.csv
 
-# curl "https://data.cityofnewyork.us/api/views/ipu4-2q9a/rows.csv?accessType=DOWNLOAD" > $FILENAME
+curl "https://data.cityofnewyork.us/api/views/ipu4-2q9a/rows.csv?accessType=DOWNLOAD" > $FILENAME
 head -1 $FILENAME  | sed "s/ /_/g" | sed "s/'//g" | sed "s/#/number/g" | sed "s/-/_/g" | sed "s/\.//g"  > $HEADERFILENAME
 head -1 $FILENAME  | sed "s/ /_/g" | sed "s/'//g" | sed "s/#/number/g" | sed "s/-/_/g" | sed "s/\.//g"  > $RESULTFILENAME
 tail +2 $FILENAME >> $RESULTFILENAME
