693833600307ebfb2f0293be908f3fa659caa3e9
Ralph Yozzo <fedex1@gmail.com>
2014-08-09 21:23:07 -0400

    fix script

--- runall.sh
+++ runall.sh
@@ -1,11 +1,15 @@
 #!
 FILENAME=dob_`date +%Y-%m-%d`.csv
 HEADERFILENAME=header_dob_`date +%Y-%m-%d`.csv
+tmpRESULTFILENAME=tmp_result_dob_`date +%Y-%m-%d`.csv
 RESULTFILENAME=result_dob_`date +%Y-%m-%d`.csv
 
 curl "https://data.cityofnewyork.us/api/views/ipu4-2q9a/rows.csv?accessType=DOWNLOAD" > $FILENAME
 head -1 $FILENAME  |sed "s/&/n/" | sed "s/Owner.s/Owners/" | sed "s/ /_/g" | sed "s/'//g" | sed "s/#/number/g" | sed "s/-/_/g" | sed "s/\.//g"  > $HEADERFILENAME
-tail +2 $FILENAME > $RESULTFILENAME
+cp $HEADERFILENAME $tmpRESULTFILENAME
+tail +2 $FILENAME >> $tmpRESULTFILENAME
+cat $tmpRESULTFILENAME | ./process.dob.to.BIGQUERY.py > $RESULTFILENAME
+
 
 # cat rows.csv | sed "s/ /_/g" | sed "s/'//g" | sed "s/#/number/g" | sed "s/-/_/g" | sed "s/\.//g" 
 
