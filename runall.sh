8b3c69d31ad101aca6e74d1bbf53b8fbe0fc1e71
Ralph Yozzo <fedex1@gmail.com>
2014-08-09 20:37:40 -0400

    fix scripts

--- runall.sh
+++ runall.sh
@@ -4,9 +4,8 @@ HEADERFILENAME=header_dob_`date +%Y-%m-%d`.csv
 RESULTFILENAME=result_dob_`date +%Y-%m-%d`.csv
 
 curl "https://data.cityofnewyork.us/api/views/ipu4-2q9a/rows.csv?accessType=DOWNLOAD" > $FILENAME
-head -1 $FILENAME  | sed "s/ /_/g" | sed "s/'//g" | sed "s/#/number/g" | sed "s/-/_/g" | sed "s/\.//g"  > $HEADERFILENAME
-head -1 $FILENAME  | sed "s/ /_/g" | sed "s/'//g" | sed "s/#/number/g" | sed "s/-/_/g" | sed "s/\.//g"  > $RESULTFILENAME
-tail +2 $FILENAME >> $RESULTFILENAME
+head -1 $FILENAME  |sed "s/Owner.s/Owners/" | sed "s/ /_/g" | sed "s/'//g" | sed "s/#/number/g" | sed "s/-/_/g" | sed "s/\.//g"  > $HEADERFILENAME
+tail +2 $FILENAME > $RESULTFILENAME
 
 # cat rows.csv | sed "s/ /_/g" | sed "s/'//g" | sed "s/#/number/g" | sed "s/-/_/g" | sed "s/\.//g" 
 
